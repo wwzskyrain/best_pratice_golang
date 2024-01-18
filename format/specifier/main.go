a29a3e18f6c646c8fda72ff63479e79b0dc44cf9
wangweizhen <wangweizhen@bytedance.com>
2024-01-18 16:21:03 +0800

    提交的本地

--- format/specifier/main.go
+++ format/specifier/main.go
@@ -7,6 +7,24 @@ type Person struct {
 	name string
 }
 
+func (p *Person) String() string {
+	// 第一次遇到go中的【stack overflow】哈哈
+	// fatal error: stack overflow
+	// return fmt.Sprintf("%+v", p)
+	return "person-string"
+}
+
+// PersonSlice 难道要给所有自定义struct都实现其对应的slice么？泛型来解决？
+type PersonSlice []*Person
+
+func (pSlice PersonSlice) String() string {
+	out := ""
+	for _, pp := range pSlice {
+		out = out + fmt.Sprintf("%+v ,", *pp)
+	}
+	return out
+}
+
 func main() {
 
 	fmt.Printf("success=%t \n", true)
@@ -15,4 +33,11 @@ func main() {
 	}
 	fmt.Printf("%+v \n", p)
 	fmt.Printf("%T \n", p)
+
+	persons := make([]*Person, 1)
+	persons = append(persons, &Person{1, "name1"})
+	persons = append(persons, &Person{2, "name2"})
+	persons = append(persons, &Person{3, "name3"})
+	fmt.Println(persons)
+	fmt.Println(PersonSlice(persons))
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/pratice/golang
 
-go 1.21.5
+go 1.17
 
 require (
 	github.com/bitly/go-simplejson v0.5.1
